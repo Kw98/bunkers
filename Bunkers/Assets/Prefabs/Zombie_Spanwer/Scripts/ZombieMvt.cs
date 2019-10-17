e6b0bc771989b0861e10e53aecf76144c8094aa1
lothaire <lothaire.noah@epitech.eu>
2019-10-17 07:57:14 +0200

    prefabs Zombie

---
+++ Bunkers/Assets/Prefabs/Zombie_Spanwer/Scripts/ZombieMvt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMvt : MonoBehaviour
+{ 
+    public GameObject player;
+    public float speed = 0;
+    private Vector3 v_diff;
+    private float atan2;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Player Position: X = " + player.transform.position.x + " --- Y = " + player.transform.position.y + " --- Z = " +
+        player.transform.position.z);
+        Chase();
+    }
+
+    void Chase()
+    {
+        Vector3 targetDirection = player.transform.position - transform.position;
+        transform.position += targetDirection * speed * Time.deltaTime;
+        v_diff = (player.transform.position - transform.position);
+        atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg);
+    }
+}
---
+++ Bunkers/Assets/Prefabs/Zombie_Spanwer/Scripts/ZombieSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawn : MonoBehaviour
+{
+    public Object zombie;
+    public float spawninterval;
+    public float nextspawn;
+
+    void Start()
+    {
+        nextspawn = Time.time + spawninterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextspawn)
+        {
+            nextspawn = Time.time + spawninterval;
+            SpawnZombie();
+        }
+    }
+
+    void SpawnZombie()
+    {
+        Instantiate(zombie, transform.position, transform.rotation); 
+    }
+}
