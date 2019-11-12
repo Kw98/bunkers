6e594dbaf9a2ec5c2b01e41c73c77c57066fc8c5
win_iiwk <alexandrecaila26@gmail.com>
2019-11-12 09:46:24 +0900

    K

---
+++ Bunkers/Assets/Network/PhotonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+    public GameObject   mainCamera;
+    public GameObject   spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("1.0");
+    }
+
+    void    OnJoinedLobby() {
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(){MaxPlayers = 4}, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom() {
+        GameObject player = PhotonNetwork.Instantiate("Player", spawnPoint.transform.position, Quaternion.identity, 0);
+        mainCamera.GetComponent<CameraFollow>().player = player;
+        player.GetComponent<PlayerAction>().camera = mainCamera.GetComponent<Camera>();
+    }
+}
---
+++ Bunkers/Assets/Prefab/Player/Scripts/PlayerAction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float    speed;
+    public Rigidbody2D  rigidbody;
+    public Camera   camera;
+    public Animator legs;
+    private Vector2     movement;
+    private Vector2         mousePos;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A))
+            gameObject.GetComponent<Inventory>().drop();
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        legs.SetFloat("Run", movement.x + movement.y);
+    }
+
+    private void FixedUpdate() {
+        rigidbody.velocity = movement;
+        Vector2 lookDir = mousePos - rigidbody.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rigidbody.rotation = angle;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Collided");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered");
+            if (other.gameObject.tag == "Weapon" || other.gameObject.tag == "Charger")
+                gameObject.GetComponent<Inventory>().loot(other.gameObject);
+    }
+}
