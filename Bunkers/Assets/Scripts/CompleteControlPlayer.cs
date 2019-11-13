cff1b199d985b072abb03d6181c3b9d33d3adeff
unknown <marie.aimee.fourtane@gmail.com>
2019-11-14 06:06:31 +0900

    7 Prefabs

---
+++ Bunkers/Assets/Scripts/CompleteControlPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteControlPlayer : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private float speedMultiplier;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * speedMultiplier, 0.8f),
+                                    Mathf.Lerp(0, Input.GetAxis("Vertical") * speedMultiplier, 0.8f));
+        print(rb2d.velocity);
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            print("bouge");
+        }
+    }
+}
