c0db2b6b69db0d8ea14d2e7a03256ee1a910074c
unknown <marie.aimee.fourtane@gmail.com>
2019-11-30 06:12:07 +0900

    minimap

---
+++ Bunkers/Assets/Script/MiniMapArrow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapArrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject parent;
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.rotation = parent.transform.rotation;
+    }
+}
