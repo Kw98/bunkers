2acbc16a1ebe3f079274382d1b050fc69266e714
unknown <marie.aimee.fourtane@gmail.com>
2019-12-01 15:28:32 +0900

    No arrow for dead zombie

--- Bunkers/Assets/Script/MiniMapArrow.cs
+++ Bunkers/Assets/Script/MiniMapArrow.cs
@@ -6,14 +6,21 @@ public class MiniMapArrow : MonoBehaviour
 {
     // Start is called before the first frame update
     private GameObject parent;
+    private ZombieMvt zombieFct;
+
     void Start()
     {
         parent = transform.parent.gameObject;
+        zombieFct = gameObject.GetComponentInParent<ZombieMvt>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.rotation = parent.transform.rotation;
+        transform.rotation = parent.transform.rotation;
+       if (zombieFct && zombieFct.dead)
+       {
+            Destroy(gameObject);
+       }
     }
 }
